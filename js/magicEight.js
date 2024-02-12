2c0391d47829e87c5ff09f7d4355acfa52e282b4
Jose M. Gomez <gomezjosem@live.com>
2024-02-11 20:00:07 -0500

    switch case script for magic 8

---
+++ js/magicEight.js
@@ -0,0 +1,39 @@
+var userName = 'Prince Charming';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
+
+var userQuestion = "Will I find true love's kiss? （づ￣3￣）づ╭❤️～";
+
+console.log(userQuestion);
+
+var randomNumber = Math.floor(Math.random() * 8);
+
+var eightBall = '';
+
+switch (randomNumber) {
+  case 1:
+    console.log("It is certain");
+    break;
+  case 2:
+    console.log("It is decidedly so");
+    break;
+  case 3:
+    console.log("Reply hazy try again");
+    break;
+  case 4:
+    console.log("Cannot predict now");
+    break;
+  case 5:
+    console.log("Do not count on it");
+    break;
+  case 6:
+    console.log("My sources say no");
+    break;
+  case 7:
+    console.log("Outlook not so good");
+    break;
+  case 8:
+    console.log("Signs point to yes");
+    break;
+  default:
+    console.log("The universe is out of order...");
+}
