fe78424132d0571dd381210ca6147c345b751626
Jose M. Gomez <gomezjosem@live.com>
2024-02-13 17:57:53 -0500

    practice sort of objects in array by property value

---
+++ js/sortByTeeth.js
@@ -0,0 +1,20 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+const sortSpeciesByTeeth = array => {
+  const sortedByTeeth = array.sort((a,b) => {
+    return a.numTeeth - b.numTeeth;
+  })
+  
+  return sortedByTeeth;
+}
+
+// Test function
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
